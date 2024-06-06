28529127926ec0a37a40be43c2148907e382ef34
Justin Mui <justin.mui25@ycdsbk12.ca>
2024-06-06 12:42:18 -0400

    Beginning Connections

--- Sketch1.java
+++ Sketch1.java
@@ -2,13 +2,39 @@ import processing.core.PApplet;
 
 public class Sketch1 extends PApplet {
 	
-	
+	String[][] incorrectGroups = {
+    {"apple", "orange", "banana", "grape"},
+    {"dog", "cat", "fish", "bird"},
+    {"car", "bus", "bike", "train"},
+    {"red", "blue", "green", "yellow"}
+  };
+
+  String[][] correctGroups = {
+    {"apple", "orange", "banana", "grape"},
+    {"dog", "cat", "fish", "bird"},
+    {"car", "bus", "bike", "train"},
+    {"red", "blue", "green", "yellow"}
+  }
+
+  selectedWords = new String [4];
+  int intNumSelected = 0;
+
+  /*
+   * Start game
+   * All boxes are drawn onto screen from incorrectGroups
+   * 4 boxes can be selected
+   * Only a max of 4 boxes can be selected, then the user must press enter
+   * The program then checks to see if the selected matches a row in the correct groups
+   * If yes, then turn those boxes blank, grey, etc
+   * Game is complete when all 4 rows are solved
+   */
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(600, 600);
   }
 
   /** 
@@ -16,7 +42,9 @@ public class Sketch1 extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255);
+    System.out.println(correctGroups);
+    System.out.println(incorrectGroups);
   }
 
   /**
@@ -25,11 +53,7 @@ public class Sketch1 extends PApplet {
   public void draw() {
 	  
 	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    
   }
   
   // define other methods down here.
